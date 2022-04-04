73c9db178347f63a0db9063012b658ebee7323cc
Jen Cho <99620655+choyoonme@users.noreply.github.com>
2022-04-04 17:58:17 -0400

    add date and time

---
+++ Develop/script.js
@@ -0,0 +1,36 @@
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+let now = moment().format("dddd, MMMM Do YYYY");
+
+$("#currentDay").text(now);
+$(".lead").add("style", "textarea");
+
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+
+let hour = moment().format("h:mm a");
+$(".container").text(hour);
+
+let timeBlock = $("<div class='time-block'><section class='row'></section></div>");
+
+$(timeBlock).appendTo(".container");
+
+
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+
+
+
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+
+
+
+// WHEN I refresh the page
+// THEN the saved events persist
